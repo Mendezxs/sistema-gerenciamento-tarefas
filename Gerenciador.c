5aeb12731194573857c39920113fa9070a7aeef5
Mendezxs <arthuraalexmendes@gmail.com>
2025-06-13 14:40:10 -0300

    Criando protótipo das funções

---
+++ Gerenciador.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void CadastrarTarefa(char Tarefas[][4][50], int *quantidade);
+void ListarTarefa(char Tarefas[][4][50], int quantidade);
+void EditarTarefa(char Tarefas[][4][50], int indice);
+void ExcluirTarefa(char Tarefas[][4][50], int *quantidade, int indice);
+void SalvarTarefasEmArquivo(char Tarefas[][4][50], int quantidade);
+
+
+int main()
+{
+    
+    
+    
+    
+    
+    return 0;
+}
